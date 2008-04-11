6b595fa842d2f6725131572c0bebbcd444c24892
Chris Heald <cheald@gmail.com>
2008-04-11 04:31:13 +0000

    Give StickyChannels config options so you can choose which channels to stick

--- Modules/StickyChannels.lua
+++ Modules/StickyChannels.lua
@@ -1,13 +1,38 @@
 local mod = Chatterbox:NewModule("StickyChannels")
 
+local channels = {
+	SAY = "Say",
+	EMOTE = "Emote",
+	YELL = "Yell",
+	OFFICER = "Officer",
+	RAID_WARNING = "Raid warning",
+	WHISPER = "Whisper",
+	CHANNEL = "Custom channels"
+}
+local options = {}
+local defaults = {profile = {}}
+
+function mod:OnInitialize()
+	self.db = Chatterbox.db:RegisterNamespace("StickyChannels", defaults)
+	for k, v in pairs(channels) do
+		defaults.profile[k] = true
+		options[k] = {
+			type = "toggle",
+			name = v,
+			desc = "Make " .. v .. " sticky",
+			get = function() return mod.db.profile[k] end,
+			set = function(info, v)
+				mod.db.profile[k] = v
+				ChatTypeInfo[k].sticky = v and 1 or 0
+			end
+		}
+	end
+end
+
 function mod:OnEnable()
-	ChatTypeInfo.SAY.sticky = 1
-	ChatTypeInfo.EMOTE.sticky = 1
-	ChatTypeInfo.YELL.sticky = 1
-	ChatTypeInfo.OFFICER.sticky = 1
-	ChatTypeInfo.RAID_WARNING.sticky = 1
-	ChatTypeInfo.WHISPER.sticky = 1
-	ChatTypeInfo.CHANNEL.sticky = 1
+	for k, v in pairs(channels) do
+		ChatTypeInfo[k].sticky = self.db.profile[k]
+	end
 end
 
 function mod:OnDisable()
@@ -17,4 +42,8 @@ function mod:OnDisable()
 	ChatTypeInfo.RAID_WARNING.sticky = 0
 	ChatTypeInfo.WHISPER.sticky = 0
 	ChatTypeInfo.CHANNEL.sticky = 0
+end
+
+function mod:GetOptions()
+	return options
 end

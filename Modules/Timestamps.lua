3f53f47e42f2ccc44715085fb05944d7eadc8b25
Chris Heald <cheald@gmail.com>
2008-04-11 04:23:53 +0000

    Add a timestamp module

---
+++ Modules/Timestamps.lua
@@ -0,0 +1,16 @@
+local mod = Chatterbox:NewModule("Timestamps", "AceHook-3.0")
+
+function mod:OnEnable()
+	for i = 1, NUM_CHAT_WINDOWS do
+		local cf = _G["ChatFrame" .. i]
+		if cf ~= COMBATLOG then
+			self:RawHook(cf, "AddMessage", true)
+		end
+	end
+end
+
+local COLOR = "777777"
+function mod:AddMessage(frame, text, ...)
+	text = "|cff"..COLOR.."["..date("%X").."]|r "..text
+	return self.hooks[frame].AddMessage(frame, text, ...)
+end

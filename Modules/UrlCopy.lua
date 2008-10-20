54345b5c2ce72ccf2253a45c7e2259b6ce0c714c
Erik Broes <erikbroes@gmail.com>
2008-10-20 17:05:40 +0000

    URLs with a '-' now get turned into proper links by UrlCopy.

--- Modules/UrlCopy.lua
+++ Modules/UrlCopy.lua
@@ -32,13 +32,13 @@ local patterns = {
 	{ pattern = "^(%a[%w%.+-]+://%S+)", matchfunc=Link},
 	{ pattern = "%f[%S](%a[%w%.+-]+://%S+)", matchfunc=Link},
 		-- www.X.Y url
-	{ pattern = "^(www%.[%w_-%%]+%.%S+)", matchfunc=Link},
-	{ pattern = "%f[%S](www%.[%w_-%%]+%.%S+)", matchfunc=Link},
+	{ pattern = "^(www%.[-%w_%%]+%.%S+)", matchfunc=Link},
+	{ pattern = "%f[%S](www%.[-%w_%%]+%.%S+)", matchfunc=Link},
 		-- "W X"@Y.Z email (this is seriously a valid email)
-	--{ pattern = '^(%"[^%"]+%"@[%w_-%%%.]+%.(%a%a+))', matchfunc=Link_TLD},
-	--{ pattern = '%f[%S](%"[^%"]+%"@[%w_-%%%.]+%.(%a%a+))', matchfunc=Link_TLD},
+	--{ pattern = '^(%"[^%"]+%"@[-%w_%%%.]+%.(%a%a+))', matchfunc=Link_TLD},
+	--{ pattern = '%f[%S](%"[^%"]+%"@[-%w_%%%.]+%.(%a%a+))', matchfunc=Link_TLD},
 		-- X@Y.Z email
-	{ pattern = "(%S+@[%w_-%%%.]+%.(%a%a+))", matchfunc=Link_TLD},
+	{ pattern = "(%S+@[-%w_%%%.]+%.(%a%a+))", matchfunc=Link_TLD},
 		-- XXX.YYY.ZZZ.WWW:VVVV/UUUUU IPv4 address with port and path
 	{ pattern = "^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d:[0-6]?%d?%d?%d?%d/%S+)", matchfunc=Link},
 	{ pattern = "%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d:[0-6]?%d?%d?%d?%d/%S+)", matchfunc=Link},
@@ -52,17 +52,17 @@ local patterns = {
 	{ pattern = "^([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%)%f[%D]", matchfunc=Link},
 	{ pattern = "%f[%S]([0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%.[0-2]?%d?%d%)%f[%D]", matchfunc=Link},
 		-- X.Y.Z:WWWW/VVVVV url with port and path
-	{ pattern = "^([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)", matchfunc=Link_TLD},
-	{ pattern = "%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)", matchfunc=Link_TLD},
+	{ pattern = "^([-%w_%%%.]+[-%w_%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)", matchfunc=Link_TLD},
+	{ pattern = "%f[%S]([-%w_%%%.]+[-%w_%%]%.(%a%a+):[0-6]?%d?%d?%d?%d/%S+)", matchfunc=Link_TLD},
 		-- X.Y.Z:WWWW url with port (ts server for example)
-	{ pattern = "^([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]", matchfunc=Link_TLD},
-	{ pattern = "%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]", matchfunc=Link_TLD},
+	{ pattern = "^([-%w_%%%.]+[-%w_%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]", matchfunc=Link_TLD},
+	{ pattern = "%f[%S]([-%w_%%%.]+[-%w_%%]%.(%a%a+):[0-6]?%d?%d?%d?%d)%f[%D]", matchfunc=Link_TLD},
 		-- X.Y.Z/WWWWW url with path
-	{ pattern = "^([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)", matchfunc=Link_TLD},
-	{ pattern = "%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+)/%S+)", matchfunc=Link_TLD},
+	{ pattern = "^([-%w_%%%.]+[-%w_%%]%.(%a%a+)/%S+)", matchfunc=Link_TLD},
+	{ pattern = "%f[%S]([-%w_%%%.]+[-%w_%%]%.(%a%a+)/%S+)", matchfunc=Link_TLD},
 		-- X.Y.Z url
-	{ pattern = "^([%w_-%%%.]+[%w_-%%]%.(%a%a+))", matchfunc=Link_TLD},
-	{ pattern = "%f[%S]([%w_-%%%.]+[%w_-%%]%.(%a%a+))", matchfunc=Link_TLD},
+	{ pattern = "^([-%w_%%%.]+[-%w_%%]%.(%a%a+))", matchfunc=Link_TLD},
+	{ pattern = "%f[%S]([-%w_%%%.]+[-%w_%%]%.(%a%a+))", matchfunc=Link_TLD},
 }
 
 do

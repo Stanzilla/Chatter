07fa9ce29f4f9a744f922477a4fadb9feff92324
Sal Scotto <sal.scotto@gmail.com>
2010-06-30 16:45:41 -0400

    Added support to AutoPopout for RealId direct whispers

--- Modules/AutoPopup.lua
+++ Modules/AutoPopup.lua
@@ -5,16 +5,24 @@ mod.modName = L["Automatic Whisper Windows"]
 function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_WHISPER","ProcessWhisper")
 	self:RegisterEvent("CHAT_MSG_WHISPER_INFORM","ProcessWhisper")
+	self:RegisterEvent("CHAT_MSG_BN_WHISPER_INFORM", "ProcessWhisper")
+	self:RegisterEvent("CHAT_MSG_BN_WHISPER","ProcessWhisper")
 end
 
 function mod:OnDisable()
 	self:UnregisterEvent("CHAT_MSG_WHISPER")
 	self:UnregisterEvent("CHAT_MSG_WHISPER_INFORM")
+	self:UnregisterEvent("CHAT_MSG_BNWHISPER")
+	self:UnregisterEvent("CHAT_MSG_BNWHISPER_INFORM")
 end
 
-function mod:ProcessWhisper(event,message,sender,language,channelString,target,flags,...)
+function mod:ProcessWhisper(event,message,sender,language,channelString,target,flags,arg7,arg8,...)
 	-- Do we have a temp window already for this target
-	if FCFManager_GetNumDedicatedFrames("WHISPER", sender) == 0 then
-		FCF_OpenTemporaryWindow("WHISPER",sender)
+	local type = "WHISPER"
+	if event == "CHAT_MSG_BN_WHISPER" or event == "CHAT_MSG_BN_WHISPER_INFORM" then
+		type = "BN_WHISPER"
+	end
+	if FCFManager_GetNumDedicatedFrames(type, sender) == 0 then
+		FCF_OpenTemporaryWindow(type,sender)
 	end
 end

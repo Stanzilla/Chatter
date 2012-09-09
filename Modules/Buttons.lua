4bf2311cf67df91e571ec4e4a481b0adaba518a5
Sal Scotto <sal.scotto@gmail.com>
2012-09-09 10:11:31 -0400

    more bug fixes

--- Modules/Buttons.lua
+++ Modules/Buttons.lua
@@ -1,4 +1,4 @@
-﻿local mod = Chatter:NewModule("Disable Buttons", "AceHook-3.0")
+local mod = Chatter:NewModule("Disable Buttons", "AceHook-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Chatter")
 
 mod.modName = L["Disable Buttons"]
